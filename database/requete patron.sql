adba8f882ad12906e10ed0a560f6ded0c384d7cf
MarinHeckmann <marin.heckmann@edu.devinci.fr>
2023-05-16 23:15:33 +0200

    end request manager sql

--- database/requete patron.sql
+++ database/requete patron.sql
@@ -1,12 +1,17 @@
 -- prix moyen bouquet standard
-SELECT AVG(standard_bouquet.price)
+SELECT SUM(standard_bouquet.price),COUNT(standard_bouquet.price)
 FROM standard_bouquet JOIN purchase_order 
 ON purchase_order.bouquet_name = standard_bouquet.name;
 
 -- prix moyen arrangement floral
-SELECT AVG(flower_arrangement.price)
+SELECT SUM(flower_arrangement.price),COUNT(flower_arrangement.price)
 FROM flower_arrangement JOIN purchase_order
 ON flower_arrangement.id=purchase_order.arrangement_id;
+
+-- prix moyen bouquet tout confondu
+SELECT (SUM(standard_bouquet.price)+SUM(flower_arrangement.price))/(COUNT(flower_arrangement.price)+COUNT(standard_bouquet.price))
+FROM standard_bouquet JOIN purchase_order JOIN flower_arrangement
+ON purchase_order.bouquet_name = standard_bouquet.name OR flower_arrangement.id=purchase_order.arrangement_id;
 
 -- client du mois
 SELECT COUNT(purchase_order.id), client.id, client.first_name, client.name
@@ -37,4 +42,18 @@ LIMIT 10;
 
 -- magasin ayant généré le plus gros chiffres d'affaire
 SELECT shop.address,SUM(standard_bouquet.price)
-FROM shop JOIN standard_bouquet
+FROM shop JOIN standard_bouquet JOIN purchase_order
+ON shop.id=purchase_order.shop_id AND purchase_order.bouquet_name=standard_bouquet.name
+GROUP BY shop.address
+ORDER BY SUM(standard_bouquet.price) DESC
+LIMIT 10;
+
+-- fleur exotique la moins vendue
+SELECT item_name, sum(quantity)
+FROM arrangement_contains JOIN item
+ON arrangement_contains.item_name=item.name
+WHERE item.type='f'
+GROUP BY item_name
+ORDER BY SUM(quantity)
+LIMIT 10;
+
