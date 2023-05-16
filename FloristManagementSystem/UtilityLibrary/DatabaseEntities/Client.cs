64af24de7ce04d36f93a0534694a4703edba2fb4
louwiiii <louisgauthier46@gmail.com>
2023-05-16 23:53:54 +0200

    Create Client.cs

---
+++ FloristManagementSystem/UtilityLibrary/DatabaseEntities/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UtilityLibrary
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string CreditCard { get; set; }
+        public DateTime InscriptionDate { get; set; }
+
+        public Client(int id, string firstName, string lastName, string email, string phoneNumber, string address, string creditCard, DateTime inscriptionDate)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            CreditCard = creditCard;
+            InscriptionDate = inscriptionDate;
+        }
+    }
+}
