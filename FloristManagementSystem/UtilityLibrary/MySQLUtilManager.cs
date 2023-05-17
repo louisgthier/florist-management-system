320a1152521f457a870c7be535aca3330067e380
louwiiii <louisgauthier46@gmail.com>
2023-05-17 07:48:10 +0200

    Export XML

--- FloristManagementSystem/UtilityLibrary/MySQLUtilManager.cs
+++ FloristManagementSystem/UtilityLibrary/MySQLUtilManager.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using UtilityLibrary;
 using MySqlX.XDevAPI.Common;
+using System.Xml;
 
 namespace UtilityLibrary
 {
@@ -55,8 +56,71 @@ namespace UtilityLibrary
                     command.Parameters.AddWithValue("@OrderId", orderId);
                     int rowsAffected = command.ExecuteNonQuery();
                 }
+            }
+        }
 
+        public static void ExportClientsToXML()
+        {
+            // SQL query to retrieve clients who have ordered more than once in the last month
+            string sqlQuery = @"
+                SELECT c.id, c.first_name, c.name, c.email, c.phone_number, c.address, c.credit_card, c.inscription_date
+                FROM client c
+                WHERE c.id IN (
+                    SELECT p.client_id
+                    FROM purchase_order p
+                    WHERE p.order_date >= DATE_SUB(NOW(), INTERVAL 1 MONTH)
+                    GROUP BY p.client_id
+                    HAVING COUNT(*) > 1
+                )";
 
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                // Open the database connection
+                connection.Open();
+
+                // Create a MySqlCommand object
+                using (MySqlCommand command = new MySqlCommand(sqlQuery, connection))
+                {
+                    // Create a DataTable to hold the result set
+                    DataTable dataTable = new DataTable();
+
+                    // Execute the query and fill the DataTable
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+
+                    // Create an XmlDocument to hold the XML data
+                    XmlDocument xmlDocument = new XmlDocument();
+
+                    // Create the root element
+                    XmlElement rootElement = xmlDocument.CreateElement("Clients");
+                    xmlDocument.AppendChild(rootElement);
+
+                    // Iterate through the rows in the DataTable
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        // Create a client element
+                        XmlElement clientElement = xmlDocument.CreateElement("Client");
+
+                        // Iterate through the columns in the row
+                        foreach (DataColumn column in dataTable.Columns)
+                        {
+                            // Create an element for each column and set its value
+                            XmlElement columnElement = xmlDocument.CreateElement(column.ColumnName);
+                            columnElement.InnerText = row[column].ToString();
+
+                            // Add the column element to the client element
+                            clientElement.AppendChild(columnElement);
+                        }
+
+                        // Add the client element to the root element
+                        rootElement.AppendChild(clientElement);
+                    }
+
+                    // Save the XML document to a file or perform any desired operations
+                    xmlDocument.Save("clients.xml");
+                }
             }
         }
     }
